aa6146d49c05eb83796e7fa048fe6fae3b8fec84
kanako001 <kunleanako@gmail.com>
2020-07-23 18:13:43 -0400

    Misc

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect} from "react-router-dom"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faTwitter, faFacebook, faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
-import {faSearch, faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import {faSearch, faBars, faShoppingCart, faPhone, faAt } from '@fortawesome/free-solid-svg-icons'
 
 
 import Home from './pages/home'
@@ -12,7 +12,7 @@ import Footer from './pages/footer'
 import Nomatch from './pages/no-match'
 import Navigation from './navigation/navigation'
 
-library.add(fab, faSearch, faBars, faShoppingCart, faFacebook, faTwitter, faInstagramSquare)
+library.add(fab, faSearch, faBars, faShoppingCart, faFacebook, faTwitter, faInstagramSquare, faPhone, faAt)
 
 export default class App extends Component {
 
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import firstImage from '../../../static/assets/images/itemImages/live/sheep2.jpg'
+import secondImage from '../../../static/assets/images/itemImages/live/goat.jpg'
 
 export default class Contact extends Component {
 
@@ -15,15 +18,25 @@ export default class Contact extends Component {
       <div className='contact-wrapper current-page'>
         <h1>Contact Us</h1>
   
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum amet aperiam fugiat! Doloremque nesciunt illum reiciendis, dolor iusto ad adipisci magni quos dicta fugit alias aliquid facilis rem recusandae explicabo?
-        Pariatur quos cupiditate ducimus, odio doloribus quo, assumenda itaque eius unde quae, corrupti numquam. Pariatur beatae eum repudiandae, aspernatur voluptatibus exercitationem inventore non tempora repellat nesciunt, earum delectus quae expedita!
-        Laborum hic consectetur molestias corporis facere deserunt, veniam iusto totam? Culpa in quia sit eius est voluptatibus, cupiditate asperiores necessitatibus? Tempore consequuntur veniam ex placeat dicta aliquam, dignissimos sapiente eum!
-        Aperiam saepe sed ipsam quos, nisi eos ea tenetur dolorem fugit at dolore. Voluptatibus placeat quaerat labore aperiam odio ab iusto fugit rerum autem nulla consequuntur, aliquam laboriosam ea nesciunt.
-        Aliquam quibusdam, expedita, deleniti molestiae quas necessitatibus inventore, eligendi quisquam recusandae assumenda deserunt perferendis doloremque eveniet rerum non corporis exercitationem voluptatibus ipsum animi et! Sequi reprehenderit modi amet recusandae perspiciatis.
-        Iste omnis, dolor consectetur aut soluta tempore laboriosam dolore perspiciatis quia ipsa distinctio maiores quasi libero veritatis corrupti deserunt animi quaerat repellat nobis inventore doloribus voluptas. Corporis consequatur est neque.
-        Sapiente voluptate aut officia voluptates nesciunt expedita eligendi repellat corrupti perspiciatis odio eum mollitia consequatur eaque vel ratione laboriosam, perferendis quisquam quos! Distinctio numquam nulla eius tempore assumenda voluptate quidem.
-        Esse aperiam dolore quae id consectetur. Beatae sed, ratione ex omnis commodi voluptas vel velit doloribus culpa quas laboriosam adipisci asperiores, esse corporis, quibusdam ducimus fugiat optio obcaecati saepe recusandae?</div>
+        <div className="contact-content-wrapper">
+          <div className="phone-wrapper">
+            <FontAwesomeIcon icon={"phone"} /> (123) 456-7898
+          </div>
+          <div className="email-wrapper">
+            <FontAwesomeIcon icon={"at"} /> anakoacres@gmail.com
+          </div>
+        </div>
+
+        <div className="image-wrapper">
+          <div className="first-image">
+            <img src={firstImage} style={{width: '350px', borderRadius: '5px'}} alt="first image"/>
+          </div>
+          <div className="second-image">
+            <img src={secondImage}  style={{width: '350px', height: '350px', borderRadius: '5px' }} alt="first image"/>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
